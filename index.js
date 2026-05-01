99941b8c0b315949b732d66f0ddf93a7e84cb640
SteakQurbanMusic <kesyayaw@gmail.com>
2026-05-01 14:48:46 +0700

    fix mongo2

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const {
   ChannelType,
 } = require("discord.js");
 
-const mongoose = require("mongoose");
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB error:", err));
